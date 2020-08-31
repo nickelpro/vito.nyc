afb0fb4f111aa75346c2949bc0140b849cf1e6b3
Vito Gamberini <vito@gamberini.email>
2020-08-31 01:17:13 -0400

    Mostly performance optimizations

--- themes/1800/assets/js/collapse.js
+++ themes/1800/assets/js/collapse.js
@@ -12,12 +12,24 @@ function collapseToggle(el) {
   }
 }
 
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
 function collapseClose(el) {
   var collapseDiv = el.parentElement.parentElement;
-  var height = collapseDiv.clientHeight;
+  var collapseTop = collapseDiv.parentElement.getElementsByClassName('collapse-button')[0];
 
-  window.scrollBy({
-    left: 0, top: -height, behavior: 'smooth'
-  });
+  if(!isInViewport(collapseTop)) {
+    window.scrollBy({
+      left: 0, top: -collapseDiv.clientHeight, behavior: 'smooth'
+    });
+  }
   collapseDiv.style.height = 0;
 }
